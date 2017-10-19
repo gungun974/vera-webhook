a6a96a9f7039181387b06208f794922d344b89f8
Gungun974 <xfelix974@gmail.com>
2017-10-19 22:20:52 +0400

    rarararar
    ttatatatarara

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+$config = [
+	'bureau' => [
+		'type' => 'ligtht',
+		'id' => 40
+	]
+];
+
+$VERA_IP = "165.169.52.244";
+$VERA_PORT = 3480;
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
@@ -7,24 +17,30 @@ if ($method === "POST") {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
-    $light_name = $json->result->parameters->light_name;
+    $name = $json->result->parameters->name;
 
     $response = new stdClass();
+    $response->source = "vera-webhook";
 
     switch ($json->result->action) {
     	case 'TurnOn':
-	    	$ch = curl_init(); 
-	        curl_setopt($ch, CURLOPT_URL, "http://165.169.52.244:3480/data_request?id=action&output_format=json&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1&DeviceNum=40"); 
-	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	        $output = curl_exec($ch); 
-	        curl_close($ch);      
-		    $response->speech = "J'allumere la lumière " . $light_name;
-		    $response->source = "vera-webhook";
+    	    if (!is_null($config[$name])) {
+    	        if ($config[$name]['type'] === "light") {
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, "http://{$VERA_IP}:{$VERA_PORT}/data_request?id=action&output_format=json&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1&DeviceNum={$config[$name]['id']}");
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    $output = curl_exec($ch);
+                    curl_close($ch);
+                    $response->speech = "J'allume la lumière " . $name;
+                }
+                    $response->speech = "Excuser moi mais " . $name . " n'est pas une lumière";
+            } else {
+                $response->speech = "Excuser moi mais la lumière " . $name . "n'est pas valide";
+            }
     		break;
     	
     	default:
-		    $response->speech = "Bonjour je suis VERA !!!";
-		    $response->source = "vera-webhook";
+		    $response->speech = "Bonjour je suis VERA!";
     }
     echo json_encode($response);
 } else {
