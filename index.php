d1e2ed7a29b2d68e2ebed999f62a99733d4c6caa
Gungun974 <xfelix974@gmail.com>
2017-10-19 21:55:46 +0400

    dadada
    dada

--- index.php
+++ index.php
@@ -8,11 +8,17 @@ if ($method === "POST") {
     $json = json_decode($requestBody);
 
     $light_name = $json->result->parameters->light_name;
+
     $response = new stdClass();
 
     switch ($json->result->action) {
     	case 'TurnOn':
-		    $response->speech = "J'allumerer la lumière " . $light_name . " dans 2000 ans ou vous me donnée 1000€ dans mon paypale : ha ha ha c'est une blage";
+	    	$ch = curl_init(); 
+	        curl_setopt($ch, CURLOPT_URL, "http://165.169.52.244:3480/data_request?id=action&output_format=json&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1&DeviceNum=40"); 
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	        $output = curl_exec($ch); 
+	        curl_close($ch);      
+		    $response->speech = "J'allumere la lumière " . $light_name;
 		    $response->source = "vera-webhook";
     		break;
     	
