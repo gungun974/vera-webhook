90901b9134d3d318e9b676f8e1542f30aff08750
Gungun974 <xfelix974@gmail.com>
2017-10-19 21:29:28 +0400

    tatatatatata
    ttaatat

--- index.php
+++ index.php
@@ -7,11 +7,19 @@ if ($method === "POST") {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
-    $text = $json->result->parameters->text;
-
+    $light_name = $json->result->parameters->light_name;
     $response = new stdClass();
-    $response->speech = "Bonjour je suis VERA !!!";
-    $response->source = "vera-webhook";
+
+    switch ($json->action) {
+    	case 'TurnOn':
+		    $response->speech = "J'allumerer la lumière " . $light_name . " dans 2000 ans ou vous me donnée 1000€ dans mon paypale : ha ha ha c'est une blage";
+		    $response->source = "vera-webhook";
+    		break;
+    	
+    	default:
+		    $response->speech = "Bonjour je suis VERA !!!";
+		    $response->source = "vera-webhook";
+    }
     echo json_encode($response);
 } else {
     echo "Method not allowed";
